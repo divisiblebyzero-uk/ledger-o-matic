c50bf3b09bec9f162a442ec325d8ed41eca2f5c8
Matthew Smalley <dev@divisiblebyzero.co.uk>
2023-11-03 16:28:24 +0000

    Added frontend again

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WelcomeScreenComponent } from './misc/welcome-screen/welcome-screen.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeScreenComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
