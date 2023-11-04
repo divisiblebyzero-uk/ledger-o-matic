127dbb9ce3e0cb58c557d09077a5a307b26807a2
Matthew Smalley <dev@divisiblebyzero.co.uk>
2023-11-04 09:54:00 +0000

    Added server side graphql

---
+++ backend/src/main/kotlin/uk/divisiblebyzero/ledger/controllers/AccountGraphQLController.kt
@@ -0,0 +1,35 @@
+package uk.divisiblebyzero.ledger.service
+
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.graphql.data.method.annotation.SchemaMapping
+import org.springframework.stereotype.Controller
+import uk.divisiblebyzero.ledger.model.Account
+import uk.divisiblebyzero.ledger.model.AccountType
+import java.util.*
+
+@Controller
+public class GraphQlController(private val accountRepository: AccountRepository) {
+
+
+
+    @QueryMapping
+    fun accounts(): List<Account> {
+        return accountRepository.findAll()
+    }
+
+    @QueryMapping
+    fun account(@Argument id: Long): Optional<Account> {
+        return accountRepository.findById(id)
+    }
+
+    @QueryMapping
+    fun topLevelAccounts(@Argument accountType: AccountType): List<Account> {
+        return accountRepository.findByParentAccountIsNullAndAccountType(accountType)
+    }
+
+    @SchemaMapping
+    fun children(parentAccount: Account): List<Account> {
+        return accountRepository.findByParentAccount(parentAccount)
+    }
+}
