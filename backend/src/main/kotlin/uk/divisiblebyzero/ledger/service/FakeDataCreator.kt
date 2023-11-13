d79f44d5a1d52b08727f6a723ab0b24876bf398f
Matthew Smalley <dev@divisiblebyzero.co.uk>
2023-11-13 07:51:33 +0000

    Added a variable income in the fake data creator to make it look more realistic

--- backend/src/main/kotlin/uk/divisiblebyzero/ledger/service/FakeDataCreator.kt
+++ backend/src/main/kotlin/uk/divisiblebyzero/ledger/service/FakeDataCreator.kt
@@ -6,6 +6,7 @@ import uk.divisiblebyzero.ledger.model.Account
 import uk.divisiblebyzero.ledger.model.AccountType
 import uk.divisiblebyzero.ledger.model.Transaction
 import java.math.BigDecimal
+import java.math.RoundingMode
 import java.time.LocalDate
 
 @Service
@@ -29,6 +30,7 @@ class FakeDataCreator(val accountRepository: AccountRepository, val accountServi
         accountRepository.save(Account("Interest", AccountType.EXPENSE))
 
         accountRepository.save(Account("Salary", AccountType.INCOME))
+        accountRepository.save(Account("Interest Income", AccountType.INCOME))
 
         accountRepository.save(Account("Credit Card", AccountType.LIABILITY))
         accountRepository.save(Account("Mortgage", AccountType.LIABILITY))
@@ -59,6 +61,21 @@ class FakeDataCreator(val accountRepository: AccountRepository, val accountServi
             date = date.plusMonths(recurringMonths)
         }
     }
+
+    fun addRecurringInterest(startDate: LocalDate, recurringDays: Long, recurringMonths: Long, description: String, interestRate: BigDecimal, debitAccountName: String, creditAccountName: String) {
+        var date: LocalDate = startDate;
+        var monthlyInterestRate = interestRate.divide(BigDecimal(12), 6, RoundingMode.HALF_UP)
+        val debitAccount = accountRepository.findOneByName(debitAccountName)
+        var balance = transactionRepository.sumDebitTransactionsByAccountAtStartOfDate(debitAccount, startDate)
+        while (date <= LocalDate.now()) {
+            val interestAmount = monthlyInterestRate.times(balance);
+            balance = balance.plus(interestAmount);
+            addTransaction(date, description, interestAmount, debitAccountName, creditAccountName)
+            date = date.plusDays(recurringDays)
+            date = date.plusMonths(recurringMonths)
+        }
+    }
+
     fun createFakeData() {
         logger.info("Saving dummy data")
         createAccounts()
@@ -73,12 +90,21 @@ class FakeDataCreator(val accountRepository: AccountRepository, val accountServi
         addTransaction(
             LocalDate.now().withDayOfYear(1),
             "Opening balance",
+            BigDecimal(12000),
+            debitAccount = "Bank Account (Savings)",
+            creditAccount = "Opening Balances"
+        )
+        addTransaction(
+            LocalDate.now().withDayOfYear(1),
+            "Opening balance",
             BigDecimal(250000),
             debitAccount = "Opening Balances",
             creditAccount = "Mortgage"
         )
 
         addRecurringTransaction(LocalDate.now().withDayOfYear(15), 0, 1, "Salary", BigDecimal(1000), debitAccount = "Bank Account (Current)", creditAccount = "Salary")
+        addRecurringInterest(LocalDate.now().withDayOfYear(20), 0, 1, "Savings Interest", BigDecimal(0.06), "Bank Account (Savings)", "Interest Income")
+
         addRecurringTransaction(LocalDate.now().withDayOfYear(15), 0, 1, "Mortgage interest", BigDecimal(100), debitAccount = "Interest", creditAccount = "Mortgage")
         addRecurringTransaction(LocalDate.now().withDayOfYear(20), 0, 1, "Mortgage monthly payment", BigDecimal(150), debitAccount = "Mortgage", creditAccount = "Bank Account (Current)")
 
